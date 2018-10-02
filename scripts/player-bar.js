a51ef6647c02d9698f3e262a5b33d7434ba00998
Elena <earagon@colgate.edu>
2018-10-02 17:39:50 -0400

    First Submission

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,7 +3,7 @@
       player.playPause();
       $(this).attr('playState', player.playState);
      });
-  };
+
 
 $('button#next').on('click', function() {
 if (player.playState !== 'playing') {return;}
@@ -19,9 +19,10 @@ $('button#previous').on('click', function() {
   if (player.playState !== 'playing') {return;}
 
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-  const previousSongIndex = (currentSongIndex - 1);
-  if (previousSongIndex <= album.songs.length){return;}
+  const previousSongIndex = currentSongIndex - 1;
 
   const previousSong = album.songs[previousSongIndex];
   player.playPause(previousSong);
 });
+
+}
